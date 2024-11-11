56f859af9d3db483c13821061f445f5a2da61f76
agate-pris <agate-pris@users.noreply.github.com>
2024-11-11 12:56:47 +0900

    Update Float32.cs

--- Runtime/Float32.cs
+++ Runtime/Float32.cs
@@ -1,5 +1,24 @@
+using System;
+using System.Runtime.CompilerServices;
+
 namespace SoftFloat1991 {
     struct Float32 {
         public uint Repr;
+
+        public bool Sign {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => (Repr >> 31) != 0;
+        }
+        public uint Exponent {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => (Repr >> 23) & 0xFF;
+        }
+        public uint Fraction {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => Repr & 0x7FFFFF;
+        }
+        public static explicit operator int(Float32 a) {
+            throw new NotImplementedException();
+        }
     }
 }
